a2f89cfee07023378ca093291567c0ac6ba1fb54
Cassia Candeias de Brito <45454456837@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-23 11:25:43 -0300

    aula while

--- aula 4/repeticao_for.php
+++ aula 4/repeticao_for.php
@@ -1,3 +1,15 @@
+<style>
+    div{
+        border: 1px solid #000;
+    }
+
+    .container{
+        display: grid;
+        grid-template-rows: auto auto;
+        grid-template-columns: auto auto auto auto auto;
+    }
+</style>
+
 <h2>Estrutura de Repetição For</h2>
 <p>
     Utilizada para repetir uma sequência de instruções
@@ -12,9 +24,14 @@
     </pre>
 </p>
 
-
+<div class="container">
 <?php
-    for($contador = 0; $contador <= 10; $contador++){
-        echo "2 x $contador = " . $contador * 2 . "<br>";
+for($contador2 = 1; $contador2 <= 10; $contador2++){
+    echo "<div>";
+    for($contador1 = 0; $contador1 <= 10; $contador1++){
+        echo "$contador2 x $contador1 = " . $contador2 * $contador1 . "<br>";
     }
+    echo "</div>";
+}
 ?>
+</div>
