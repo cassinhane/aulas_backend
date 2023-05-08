d74b1a17f1f86f1a7f5cd2f07c32db50257a4f1c
Cassia Candeias de Brito <45454456837@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-08 08:41:02 -0300

    Aula 18

---
+++ aula 18/produto/excluir.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once "../conexao.php";
+
+    //verifica se foi enviado o param id pela URL
+    if(isset($_GET['id']))
+    {
+
+    //pega o valor do id que foi enviado pela URL
+    $id = $_GET['id'];
+
+    //string com o comando sql para sr executado no db
+    $sql = "DELETE FROM `produto` WHERE  `idproduto`=?;";
+    
+    //prepara o sql para ser executado no banco de dados
+    $comando = $conexao->prepare($sql);
+
+    //adiciona os valores nos parâmetros
+    $comando->bind_param("i", $id);
+    //string, string, decimal, string
+
+    //executa o sql - comando no banco de dados
+    $comando->execute();
+    }
+    //abre o arquivo form.php
+    header("Location: form.php");
+    
+?>
