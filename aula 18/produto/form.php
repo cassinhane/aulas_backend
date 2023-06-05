5dfb2e3df2c6edc40aff5fd15c28bc751556858d
Cassia Candeias de Brito <45454456837@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-06-05 08:41:07 -0300

    Aula 18

--- aula 18/produto/form.php
+++ aula 18/produto/form.php
@@ -1,6 +1,8 @@
+<?php require_once "../controla_sessao/controla.php"; ?>
 <?php require_once "consultar_por_id.php" ?>
 <?php require_once "../template/cabecalho.php";  ?>
 
+<div>
     <h1>Cadastro de Produto</h1>
     <hr>
 
@@ -10,21 +12,21 @@
     enctype="multipart/form-data"
     >
 
-       <label for="nome">Nome</label><br>
+       <label for="nome" >Nome</label><br>
        <input type="text" name="nome" id="nome" value="<?php echo $produto['nome'] ?? ""; ?>"><br>
        <input type="hidden" name="id" id="id" value="<?php echo $produto['idproduto'] ?? ""; ?>"><br>
 
-       <label for="descricao">Descrição</label><br>
+       <label for="descricao" >Descrição</label><br>
        <textarea  name="descricao" id="descricao"><?php echo $produto['descricao'] ?? ""; ?></textarea><br>
 
-       <label for="preco">Preço</label><br>
+       <label for="preco" >Preço</label><br>
        <input type="number" step="0.01" name="preco" id="preco" value="<?php echo $produto['preco'] ?? "0.00"; ?>"><br>
 
-       <label for="foto">Foto</label><br>
+       <label for="foto" >Foto</label><br>
        <input type="file" name="foto" id="foto"><br>
 
        <br>
-       <button type="submit">Cadastrar</button>
+       <button type="submit" >Cadastrar</button>
 
     </form>
 
--- aula 18/site/index.php
+++ aula 18/site/index.php
@@ -22,7 +22,7 @@
               $categorias[] = $value["categoria"];
         ?>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="index.php?categoria=<?= $value["categoria"]; ?>">
                <?php echo $value["categoria"]; ?>
             </a>
           </li>
